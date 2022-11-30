f5af7f959806bed518a09a2a54c9996192f224c5
jpiazzal <joel.piazzalungalerat@gmail.com>
2022-11-30 16:32:41 +0100

    fix(bin): Fix binary to be functional

--- bin/ts2doc.js
+++ bin/ts2doc.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
+
 const ts2doc = require('../dist');
 
 const arguments = process.argv.slice(2);
 
-const doc = ts2doc(arguments);
+const doc = ts2doc.default(arguments);
 
 console.info(doc);
