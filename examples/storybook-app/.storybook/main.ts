248927df8ffe20600a8ece5b34f5a1127153ab41
GONZALEZ Adrian <pablo.gonzalez@laposte.fr>
2024-10-02 21:23:53 +0200

    upgrade to storybook 8

---
+++ examples/storybook-app/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    {
+        name: '@ts2doc/storybook-addon',
+        options: {
+            patternDocType: 'src/**/*.ts'
+        }
+    }
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
